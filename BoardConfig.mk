a050a0e513ed148741e62f6b288f96c832ac9872
Inside™ <113822129+inside91@users.noreply.github.com>
2023-06-12 17:15:29 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -200,7 +200,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 TW_USE_FSCRYPT_POLICY := 1
-TW_LOAD_VENDOR_MODULES := "texfat.ko sec_touchscreen.ko sec_touchscreen_2.ko"
+TW_LOAD_VENDOR_MODULES := "texfat.ko sec_touchscreen.ko sec_touchscreen_2.ko et580-int.ko tcs3490.ko"
 
 # TWRP Debug Flags
 TARGET_USES_LOGD := true
